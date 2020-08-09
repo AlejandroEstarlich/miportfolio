bd0f5462def27a8174b29593e374d129edc948a7
Alejandro Estarlich <hello@alejandroestarlich.es>
2020-08-09 21:07:49 +0200

    Bloqs

--- routes/user.js
+++ routes/user.js
@@ -7,7 +7,7 @@ var router = express.Router();
 var md_auth = require('../middlewares/authenticated');
 
 
-router.post('/register', UserController.saveUser);
+// router.post('/register', UserController.saveUser);
 router.post('/login', UserController.loginUser);
 router.get('/user/:id', md_auth.ensureAuth, UserController.getUser);
 
